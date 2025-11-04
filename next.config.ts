fafafc3921c8793dd704fa00f774524543047e79
Bhanuteja <nallamothubhanuteja110@gmail.com>
2025-11-04 15:26:34 +0000

    https://swecha.org/sites/default/files/Swecha-Telangana-Logo-124w.png th

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'swecha.org',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Navbar() {
   return (
     <header className="w-full p-4 bg-gradient-to-r from-teal-200 to-cyan-200">
       <div className="container flex items-center">
         <Link href="/" className="flex items-center space-x-2">
-          {/* This is a placeholder for the logo in the image. */}
-          <span className="font-bold text-lg">Swecha</span>
+          <Image
+            src="https://swecha.org/sites/default/files/Swecha-Telangana-Logo-124w.png"
+            alt="Swecha Logo"
+            width={124}
+            height={41}
+          />
         </Link>
       </div>
     </header>
