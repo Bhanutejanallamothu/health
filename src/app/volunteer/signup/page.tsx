f51181c30efc3e29f16928e4f58488c1eefc9871
Bhanuteja <nallamothubhanuteja110@gmail.com>
2025-11-08 04:48:42 +0000

    remove all the other content in the signup page and keep only the fom

--- src/app/volunteer/signup/page.tsx
+++ src/app/volunteer/signup/page.tsx
@@ -2,7 +2,7 @@ import { VolunteerSignupForm } from "@/components/volunteer/volunteer-signup-for
 
 export default function VolunteerSignupPage() {
   return (
-    <div className="container py-12 md:py-20 flex items-center justify-center">
+    <div className="container min-h-screen py-12 md:py-20 flex items-center justify-center">
         <VolunteerSignupForm />
     </div>
   );
